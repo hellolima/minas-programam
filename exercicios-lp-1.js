8dc04cf27825fe943fcbf8d113609bbc1dad980a
Gabriella Araujo <gabrielladelimadev@gmail.com>
2021-10-17 20:39:59 -0300

    Adicionando primeira atividade de lógica de programação (arquivo de js):SPARKLES:

---
+++ exercicios-lp-1.js
@@ -0,0 +1,18 @@
+/* Armazene uma multiplicação entre dois números e mostre no console.log(); */
+var num1 = 10;
+var num2 = 50;
+
+resultado = num1 * num2;
+console.log("Resultado da multiplicação entre 10 e 50: ", resultado);
+
+/* Armazene na váriavel nomeColaboradora o nome "Maria", em seguida, utilize o console.log() para mostrar o valor da variável.*/
+var nomeColaboradora = "Maria";
+console.log("O nome da nossa colaboradora é: ", nomeColaboradora);
+
+/* Armazene separadamente em variáveis, todos os tipos de dados que aprendeu até hoje.*/
+var minhaIdade = 17;
+var meuNome = "Gabriella";
+var sobreMim = "Meu nome é Gabriella, tenho 17 anos e sou mineira da gema!!! Até que minhas notas na escola são boas e, até então kkkkkkk, não tenho filhos";
+var mediaEscola = 87.9;
+var temFilhos = false;
+/* Tentei juntar tudo em um console.log, mas não consegui concatenar =(*/
